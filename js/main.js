c5e651072ad8c5c55ffbfbf279d22c844992012c
Andrej-Nil <and.kucheroff@yandex.ru>
2021-04-13 15:22:37 +0500

    Описал логику появления модульных окон, открывание закрывание подменю, скролл страницы ввер по нажатию кнопки

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 'use strict';
 const body = document.querySelector('body');
 const app = document.querySelector('#app');
+
+//modal
 const modalsWrap = app.querySelector('#modalsWrap');
 const feetbackBtn = app.querySelector('#feetbackBtn');
+const fastOrdenBtns = app.querySelectorAll('.js-fast-order');
 const feetbackModal = app.querySelector('#feetback');
+const fastOrderModal = app.querySelector('#fastOrder');
 const modalCloseBtns = app.querySelectorAll('.js-close-modal');
+const ordenBtn = app.querySelector('#orderBtn');
+const ordenModal = app.querySelector('#order');
+
+//mobileMenu
 const mobileMenuBtn = app.querySelector('#menuBtn');
 const mobileMenu = app.querySelector('#mobileMenu');
 const mobileMenuCloseBtn = app.querySelector('#mobileMenuClose');
+
+
+const upwardBtn = app.querySelector('#upwardBtn');
+const upBtn = app.querySelector('#up');
+
+const dropdownsBtn = app.querySelectorAll('.js-dropdown-btn');
+
+let timeOut;
 
 if (mobileMenuBtn) {
   mobileMenuBtn.addEventListener('click', mobileMenuOpen);
@@ -17,26 +33,58 @@ if (mobileMenuCloseBtn) {
   mobileMenuCloseBtn.addEventListener('click', mobileMenuClose);
 }
 
+//modals
 if (feetbackModal && modalsWrap) {
-  feetbackBtn.addEventListener('click', feetbackOpen);
+  feetbackBtn.addEventListener('click', () => {
+    modalOpen(feetbackModal);
+  });
 }
 
-if (modalCloseBtns) {
+if (fastOrderModal && modalsWrap) {
+  Array.from(modalCloseBtns).forEach((btn) => {
+    btn.addEventListener('click', () => modalClose(btn));
+
+  });
+}
+
+if (fastOrdenBtns) {
+  Array.from(fastOrdenBtns).forEach((btn) => {
+    btn.addEventListener('click', () => modalOpen(fastOrderModal));
+  });
+}
+
+if (ordenBtn) {
+  ordenBtn.addEventListener('click', () => modalOpen(ordenModal));
+}
+
+if (ordenModal && modalsWrap) {
   Array.from(modalCloseBtns).forEach((btn) => {
     btn.addEventListener('click', () => modalClose(btn));
   });
 }
 
 
+// modals end
 
+if (upwardBtn) {
+  upwardBtn.addEventListener('click', goTop);
+  window.addEventListener('scroll', showUpBtn);
+}
+
+
+if (dropdownsBtn) {
+  Array.from(dropdownsBtn).forEach((btn) => {
+    btn.addEventListener('click', () => dropdownOpen(btn));
+    btn.addEventListener('click', () => rotateArrowDropdownsBtn(btn));
+  });
+}
 
 
 //Функции
-
-function feetbackOpen() {
+function modalOpen(modal) {
   body.classList.add('no-scroll');
   modalsWrap.classList.add('modals--is-show');
-  feetbackModal.classList.add('modal--is-show');
+  modal.classList.add('modal--is-show');
 }
 
 function modalClose(btn) {
@@ -53,3 +101,45 @@ function mobileMenuOpen() {
 function mobileMenuClose() {
   mobileMenu.classList.remove('mobile-menu--is-open');
 }
+
+
+function goTop() {
+  let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+  if (top > 0) {
+    window.scrollBy(0, -150);
+    timeOut = setTimeout('goTop()', 5);
+  } else clearTimeout(timeOut);
+}
+
+function showUpBtn() {
+  const coord = body.getBoundingClientRect();
+  if (coord.top < -1000) {
+    upBtn.classList.add('up--is-show');
+  } else {
+    upBtn.classList.remove('up--is-show');
+  }
+}
+
+function dropdownOpen(btn) {
+  const dropdown = btn.closest('.js-dropdown');
+  const dropdownBody = dropdown.querySelector('.js-dropdown-body');
+  const dropdownContent = dropdown.querySelector('.js-dropdown-content');
+  const dropdownContentHeight = dropdownContent.offsetHeight;
+  if (dropdownBody.offsetHeight == 0) {
+    dropdownBody.style.height = dropdownContentHeight + 'px';
+  } else {
+    dropdownBody.style.height = 0;
+  }
+
+}
+
+function rotateArrowDropdownsBtn(btn) {
+  const arrow = findChildren(btn, '.category__arrow');
+  arrow.classList.toggle('category__arrow--is-up')
+}
+
+function findChildren(el, domClass) {
+  const children = el.querySelector(domClass);
+  return children;
+}
+
